6793f5650f7a33dc78ec55b271268afe20e03dac
le15874811343 <576169235@qq.com>
2016-09-15 17:37:32 +0800

    汽车图片信息服务接口

--- 前台/S4SSHib/src/cn/com/service/ICarImagesInfoService.java
+++ 前台/S4SSHib/src/cn/com/service/ICarImagesInfoService.java
@@ -3,10 +3,28 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 汽车图片信息服务接口
+ * @author lej
+ */
 public interface ICarImagesInfoService {
+  /**
+   * 根据编号获取汽车照片的服务
+   * @parma carInfo
+   * @return Map<Integer,String>
+   */
 	public Map<Integer, String> getCarImagesInfoByID(Carinfo carInfo);
+/**
+ * 添加照片信息的服务
+ * @parma carImagesInfo
+ *@reutn boolean 
+ */
 	public boolean addCarImagesInfo(Imagesinfo carImagesInfo);
+/**
+ *修改照片信息的服务
+ * @parmas
+ * @return boolean
+ */
 	public boolean updateCarImagesInfo(Imagesinfo carImagesInfo);
 	
 }
