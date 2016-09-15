8d36cc1e2ce2a863323c0be5076c166202498714
le15874811343 <576169235@qq.com>
2016-09-15 13:59:33 +0800

    车系信息操作接口

--- 前台/S4SSHib/src/cn/com/dao/IModelDao.java
+++ 前台/S4SSHib/src/cn/com/dao/IModelDao.java
@@ -1,6 +1,15 @@
 package cn.com.dao;
 import java.util.*;
 import cn.com.pojo.*;
+/**
+ * 车系信息操作接口
+ * @author lej
+ */
 public interface IModelDao {
+          /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
       public List<Model> getModelByWhere(Model model);
 }
