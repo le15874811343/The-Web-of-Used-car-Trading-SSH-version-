a890c21d90dbdea678345c3d1ae4ed2e6fa3746e
le15874811343 <576169235@qq.com>
2016-09-15 17:48:40 +0800

    手续信息服务接口

--- 前台/S4SSHib/src/cn/com/service/IProcedureInfoService.java
+++ 前台/S4SSHib/src/cn/com/service/IProcedureInfoService.java
@@ -3,9 +3,33 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
+/**
+ * 手续信息服务接口
+ * @author lej
+ */
 public interface IProcedureInfoService {
+  /**
+   * 
+   * 获取所有手续信息的服务
+   *@return Map<Long,ProcedureInfo> 
+   */
 	public Map<Long, Procedureinfo> getAllProcedureInfo();
+  /**
+   * 
+   * 根据编号获取手续信息的服务
+   *@return ProcedureInfo
+   */
 	public Procedureinfo getProcedureInfoById(Carinfo carInfo);
+  /**
+   * 
+   * 添加手续信息的服务
+   *@return boolean
+   */
 	public boolean addProcedureInfo(Procedureinfo procedureInfo);
+  /**
+   * 
+   * 修改手续信息的服务
+   *@return  boolean
+   */
 	public boolean updateProcedureInfo(Procedureinfo procedureInfo);
 }
