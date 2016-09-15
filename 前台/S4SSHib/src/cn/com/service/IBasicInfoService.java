1100c1ec37284f380f8995d41d38136f84b9058d
le15874811343 <576169235@qq.com>
2016-09-15 17:32:38 +0800

    汽车基本信息服务接口

--- 前台/S4SSHib/src/cn/com/service/IBasicInfoService.java
+++ 前台/S4SSHib/src/cn/com/service/IBasicInfoService.java
@@ -3,10 +3,29 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 汽车基本信息服务接口
+ * @author lej
+ */
 public interface IBasicInfoService {
+/**
+ * 获取所有基本信息的服务
+ * @return Map<Long, BasicInfo>
+ */
 	public Map<Long, Basicinfo> getAllBasic();
+/**
+ *根据编号获取汽车基本信息的服务
+ *@return BasicInfo
+ */
 	public Basicinfo getAllBasicById(Carinfo carInfo);
+/**
+ * 添加汽车基本信息的服务
+ * @return boolean
+ */
 	public boolean addBasicInfo(Basicinfo basicInfo);
+/**
+ * 修改汽车基本信息的服务
+ * @return boolean
+ */
 	public boolean updateBasicInfo(Basicinfo basicInfo);
 }
