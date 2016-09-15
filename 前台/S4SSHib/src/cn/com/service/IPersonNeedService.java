42d201b661979b0b759b46d526ed6160ef624f59
le15874811343 <576169235@qq.com>
2016-09-15 17:47:06 +0800

    个人需求信息服务接口

--- 前台/S4SSHib/src/cn/com/service/IPersonNeedService.java
+++ 前台/S4SSHib/src/cn/com/service/IPersonNeedService.java
@@ -1,9 +1,27 @@
 package cn.com.service;
 
 import cn.com.pojo.*;;
-
+/**
+ * 个人需求信息服务接口
+ *@author 
+ */
 public interface IPersonNeedService {
+  /**
+   * 
+   * 添加个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean addPersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的服务
+   *@return boolean 
+   */
 	public boolean deletePersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 获取个人需求信息的服务
+   *@return PersonNeed
+   */
 	public Personneed getPerSonNeed(Personneed personNeed);
 }
