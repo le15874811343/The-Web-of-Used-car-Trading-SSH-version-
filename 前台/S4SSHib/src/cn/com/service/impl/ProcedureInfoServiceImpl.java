2edc60baba094845acec60e197c6de1a70269257
le15874811343 <576169235@qq.com>
2016-09-15 18:33:10 +0800

    手续信息服务实现类

--- 前台/S4SSHib/src/cn/com/service/impl/ProcedureInfoServiceImpl.java
+++ 前台/S4SSHib/src/cn/com/service/impl/ProcedureInfoServiceImpl.java
@@ -7,8 +7,12 @@ import cn.com.pojo.*;
 import cn.com.dao.IProcedureInfoDao;
 import cn.com.dao.impl.ProcedureInfoDaoImpl;
 import cn.com.service.IProcedureInfoService;
-
+/**
+ * 手续信息服务实现类
+ * @author lej
+ */
 public class ProcedureInfoServiceImpl implements IProcedureInfoService{
+	//手续信息操作接口的引用
 private IProcedureInfoDao procedureInfoDaoImpl=null;
 	public IProcedureInfoDao getProcedureInfoDaoImpl() {
 	return procedureInfoDaoImpl;
@@ -17,19 +21,31 @@ private IProcedureInfoDao procedureInfoDaoImpl=null;
 public void setProcedureInfoDaoImpl(IProcedureInfoDao procedureInfoDaoImpl) {
 	this.procedureInfoDaoImpl = procedureInfoDaoImpl;
 }
-
+ /**
+   * 
+   * 获取所有手续信息的服务
+   *@return Map<Long,ProcedureInfo> 
+   */
 	@Override
 	public Map<Long, Procedureinfo> getAllProcedureInfo() {
 		// TODO Auto-generated method stub
 		return procedureInfoDaoImpl.getAllProcedureInfo();
 	}
-
+  /**
+   * 
+   * 根据编号获取手续信息的服务
+   *@return ProcedureInfo
+   */
 	@Override
 	public Procedureinfo getProcedureInfoById(Carinfo carInfo) {
 		// TODO Auto-generated method stub
 		return procedureInfoDaoImpl.getProcedureInfoById(carInfo);
 	}
-
+ /**
+   * 
+   * 添加手续信息的服务
+   *@return boolean
+   */
 	@Override
 	public boolean addProcedureInfo(Procedureinfo procedureInfo) {
 		// TODO Auto-generated method stub
@@ -39,7 +55,11 @@ public void setProcedureInfoDaoImpl(IProcedureInfoDao procedureInfoDaoImpl) {
 		}
 		return flag;
 	}
-
+ /**
+   * 
+   * 修改手续信息的服务
+   *@return  boolean
+   */
 	@Override
 	public boolean updateProcedureInfo(Procedureinfo procedureInfo) {
 		// TODO Auto-generated method stub
