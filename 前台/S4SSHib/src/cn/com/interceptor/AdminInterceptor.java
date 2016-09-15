93e218959079f3d5da1e9bc6e2ee09bf6783a4c4
le15874811343 <576169235@qq.com>
2016-09-15 23:36:11 +0800

    权限拦截器

--- 前台/S4SSHib/src/cn/com/interceptor/AdminInterceptor.java
+++ 前台/S4SSHib/src/cn/com/interceptor/AdminInterceptor.java
@@ -4,7 +4,10 @@ import java.util.Map;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
-
+/**
+ * 权限拦截器
+ * @author lej
+ */
 public class AdminInterceptor extends AbstractInterceptor {
 
 	@Override
@@ -13,9 +16,9 @@ public class AdminInterceptor extends AbstractInterceptor {
 		String res=null;
 		Map<String, Object> session=arg0.getInvocationContext().getSession();
 		Object obj=session.get("userinfo");
-		if(obj!=null)
+		if(obj!=null)   //验证session中用户对象是否为空
 		{
-			res=arg0.invoke();
+			res=arg0.invoke();  //不为空则通过
 		}
 		else{
 			
