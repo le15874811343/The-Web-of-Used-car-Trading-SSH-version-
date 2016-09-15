5e43a01de774a31514f3d0481c73101b8e82aab3
le15874811343 <576169235@qq.com>
2016-09-15 17:36:33 +0800

    品牌信息服务接口

--- 前台/S4SSHib/src/cn/com/service/ICarBrandService.java
+++ 前台/S4SSHib/src/cn/com/service/ICarBrandService.java
@@ -2,10 +2,30 @@ package cn.com.service;
 import cn.com.pojo.*;
 
 import java.util.*;
-
+/**
+ * 品牌信息服务接口
+ * @author lej
+ */
 public interface ICarBrandService {
+  /**
+   * 按热度获取品牌信息的服务
+   * @return  Map<Integer, CarBrand>
+   */
 	public Map<Integer, Carbrand> getCarBrandByCount();
+  /**
+   * 获取所有品牌信息的服务
+   * @return Map<Integer,CarBrand>
+   */
 	public Map<Integer, Carbrand> getAllBrand();
+ /**
+   * 根据品牌编号获取品牌信息的服务
+   * @param carBrand
+   * @return CarBrand
+   */
 	public Carbrand getBrandByID(Carbrand carBrand);
+  /**
+   * 根据品牌名称获取品牌的服务
+   *@return CarBrand 
+   */
 	public Carbrand getBrandByName(Carbrand carBrand);
 }
