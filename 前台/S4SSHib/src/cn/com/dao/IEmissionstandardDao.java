38d034a7a890f7601d2b7c945f48677bcec582ad
le15874811343 <576169235@qq.com>
2016-09-15 13:57:23 +0800

    排放标准操作接口

--- 前台/S4SSHib/src/cn/com/dao/IEmissionstandardDao.java
+++ 前台/S4SSHib/src/cn/com/dao/IEmissionstandardDao.java
@@ -2,6 +2,15 @@ package cn.com.dao;
 
 import java.util.*;
 import cn.com.pojo.*;
+/**
+ * 
+ * 排放标准操作接口
+ *@author  lej
+ */
 public interface IEmissionstandardDao {
+	/**
+	 * 按热度获取排放标准信息的方法
+	 * @return  Map<Integer,Emissionstandard>
+	 */
 	public Map<Integer,Emissionstandard> getEmissionstandardByCount();
 }
