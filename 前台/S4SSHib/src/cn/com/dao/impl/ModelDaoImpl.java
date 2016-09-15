9ef473fc2cfd36c94b2214524c3880d4704eb35b
le15874811343 <576169235@qq.com>
2016-09-15 16:47:07 +0800

    车系信息操作实现类

--- 前台/S4SSHib/src/cn/com/dao/impl/ModelDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/ModelDaoImpl.java
@@ -4,15 +4,23 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
+/**
+ * 车系信息操作实现类
+ * @author lej
+ */
 public class ModelDaoImpl extends BaseDao implements IModelDao {
-
+      /**
+       * 按条件获取车系信息的方法
+       * @parma model
+       *@return List<Model> 
+       */
 	@Override
 	public List<Model> getModelByWhere(Model model) {
 		// TODO Auto-generated method stub
 		List<Model> modelMap=new ArrayList<Model>();
 		StringBuffer sql=new StringBuffer(" from Model where 1=1 ");
 		List<Object> parmas=new ArrayList<Object>();
-		
+		 //动态参数准备并延伸HQL语句
 		if(model.getBId()!=0){
 			sql.append("and BId=?");
 			parmas.add(model.getBId());
@@ -32,7 +40,7 @@ public class ModelDaoImpl extends BaseDao implements IModelDao {
 		
 
 	try {
-		modelMap=	super.getHibernateTemplate().find(sql.toString(),o);
+		modelMap=	super.getHibernateTemplate().find(sql.toString(),o);  //获取结果集
 
 	
 	} catch (Exception e) {
