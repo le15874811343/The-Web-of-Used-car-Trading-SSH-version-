623a77c938802a539fedf9d7f22dabb697c50e56
le15874811343 <576169235@qq.com>
2016-09-15 16:42:40 +0800

    行驶距离操作实现类

--- 前台/S4SSHib/src/cn/com/dao/impl/DistanceDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/DistanceDaoImpl.java
@@ -5,9 +5,16 @@ import java.util.*;
 import cn.com.pojo.*;
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 行驶距离操作实现类
+ *@author lej 
+ */
 public class DistanceDaoImpl extends BaseDao implements IDistanceDao {
-
+   /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
 	@Override
 	public Map<Integer, Distance> getDistanceByCount() {
 		// TODO Auto-generated method stub
@@ -16,8 +23,9 @@ public class DistanceDaoImpl extends BaseDao implements IDistanceDao {
 		String hql = "from Distance order by DCount desc";
 
 		try {
-			List<Distance> dislist = PageUtil.querylist(1, 4, hql, null);
+			List<Distance> dislist = PageUtil.querylist(1, 4, hql, null);//获取指定行数区间的结果集
 			;
+			//遍历结果集，加入map中
 			for (Distance d : dislist) {
 				distanceMap.put(d.getDId(), d);
 			}
