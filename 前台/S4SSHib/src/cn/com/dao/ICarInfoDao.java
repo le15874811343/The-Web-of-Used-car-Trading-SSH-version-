6666c2512e8d2f89a426a931950d2c52dde1a026
le15874811343 <576169235@qq.com>
2016-09-15 14:15:59 +0800

    汽车概要信息操作接口

--- 前台/S4SSHib/src/cn/com/dao/ICarInfoDao.java
+++ 前台/S4SSHib/src/cn/com/dao/ICarInfoDao.java
@@ -9,49 +9,49 @@ public interface ICarInfoDao {
   /**
  * 按热度获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo> getCarInfoByCountDesc(Carinfo carInfo);
     /**
  * 按热度和品牌获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo>  getCarInfoByBrandCountDesc(Carinfo carInfo);
    /**
  * 按上架时间获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo> getCarInfoBySjTime(Carinfo carInfo);
     /**
  * 获取四辆最新上架的汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo> getFourthCarInfoBySjTime(Carinfo carInfo);
     /**
  * 按热度和车型获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo>  getCarInfoByTypeCountDesc(Carinfo carInfo);
    /**
  * 按品牌获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo> getCarInfoByBrand(Carinfo carInfo);
    /**
  * 按车型获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long,Carinfo>   getCarInfoByType(Carinfo carInfo);
    /**
  * 按条件获取汽车概要信息的方法
  * @parma carInfo
- * @return   Map<Long, CarInfo>
+ * @return   Map<Long, Carinfo>
  */
   public Map<Long, Carinfo> getCarByWhere(Carinfo carInfo);
   /**
@@ -69,7 +69,7 @@ public interface ICarInfoDao {
    /**
  * 按唯一条件获取汽车概要信息的方法
  * @parma carInfo
- * @return   CarInfo
+ * @return   Carinfo
  */
   public Carinfo getCarInfoByUMN(Carinfo carInfo);
    /**
@@ -81,13 +81,13 @@ public interface ICarInfoDao {
     /**
    * 获取热度最高的10辆车
    * @parma carInfo
-   * @return List<CarInfo>
+   * @return List<Carinfo>
    */
   public List<Carinfo> getTenCount(Carinfo carInfo);
    /**
    * 获取热度最高10个品牌的车
    * @parma carInfo
-   * @return List<CarInfo>
+   * @return List<Carinfo>
    */
   public List<Carinfo> getTenBrandCar(Carinfo carInfo);
 }
