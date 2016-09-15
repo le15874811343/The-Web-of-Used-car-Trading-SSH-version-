30737ae7e436dd5feebd431cd023f861644a01de
le15874811343 <576169235@qq.com>
2016-09-15 18:36:37 +0800

    公司动态消息服务实现类

--- 前台/S4SSHib/src/cn/com/service/impl/TrendsServiceImpl.java
+++ 前台/S4SSHib/src/cn/com/service/impl/TrendsServiceImpl.java
@@ -6,8 +6,14 @@ import cn.com.pojo.Trends;
 import cn.com.dao.impl.*;
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 公司动态消息服务实现类
+ * @author
+ */
 public class TrendsServiceImpl implements ITrendsService,IPageDao{
+	//公司动态消息操作接口的引用
  private  ITrendsDao trendsDao=null;
+ //分页处理操作接口的引用
  private IPageDao pageTrends=null;
 	public ITrendsDao getTrendsDao() {
 	return trendsDao;
@@ -21,6 +27,12 @@ public IPageDao getPageTrends() {
 public void setPageTrends(IPageDao pageTrends) {
 	this.pageTrends = pageTrends;
 }
+
+       /**
+        * 获取指定条数的最新的动态消息集合
+        * @param  rows 获取记录的条数
+        * @return List<Trends> 
+        */
 	@Override
 	public List<Trends> getITrendsByTime(Trends trends,int rows) {
 		// TODO Auto-generated method stub
@@ -39,17 +51,32 @@ public void setPageTrends(IPageDao pageTrends) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	/**
+	 * 获取公司动态消息总记录数
+	 * @return int
+	 */
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return pageTrends.queryPersonCarCount(object);
 	}
+	/**
+	 * 分页获取公司动态消息
+	 * @param curPage 当前页
+	 *@param rowsPrePage
+	 * @return Map<Long,Object>
+	 */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return pageTrends.showPersonCarList(curPage, rowsPrePage, object);
 	}
+       /**
+        * 按条件获取动态消息
+        * 
+        * Trends
+        */
 	@Override
 	public Trends getTrendsByWhere(Trends trends) {
 		// TODO Auto-generated method stub
