354742a75196491a00145ebf1bf9018419a3b759
le15874811343 <576169235@qq.com>
2016-09-15 16:43:51 +0800

    排放标准操作实现类

--- 前台/S4SSHib/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/EmissionstandardDaoImpl.java
@@ -5,8 +5,16 @@ import java.util.*;
 import cn.com.pojo.*;
 import cn.com.dao.*;
 import cn.com.util.*;
+/**
+ * 
+ * 排放标准操作实现类
+ *@author  lej
+ */
 public class EmissionstandardDaoImpl extends BaseDao implements IEmissionstandardDao {
-
+        /**
+	 * 按热度获取排放标准信息的方法
+	 * @Map<Integer,Emissionstandard>
+	 */
 	@Override
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount() {
 		// TODO Auto-generated method stub
@@ -14,7 +22,8 @@ public class EmissionstandardDaoImpl extends BaseDao implements IEmissionstandar
 		
            String hql="from  Emissionstandard  order by ECount desc";
 	try {
-		List<Emissionstandard> dislist=	PageUtil.querylist(1, 5, hql, null);
+		List<Emissionstandard> dislist=	PageUtil.querylist(1, 5, hql, null); //获取指定行数区间的结果集
+		//遍历结果集，加入map中
 		for(Emissionstandard d: dislist){
 			emissionstandardMap.put(d.getEId(), d);
 			}
