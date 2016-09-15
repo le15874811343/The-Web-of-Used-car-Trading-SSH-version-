cace99bf3ba4bdeda19631c40d35fbc5a117868d
le15874811343 <576169235@qq.com>
2016-09-15 17:44:12 +0800

    车系信息服务接口

--- 前台/S4SSHib/src/cn/com/service/IModelService.java
+++ 前台/S4SSHib/src/cn/com/service/IModelService.java
@@ -3,7 +3,15 @@ package cn.com.service;
 import java.util.*;
 
 import cn.com.pojo.*;
-
+/**
+ * 车系信息服务接口
+ * @author lej
+ */
 public interface IModelService {
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 	public List<Model> getModelByWhere(Model model);
 }
