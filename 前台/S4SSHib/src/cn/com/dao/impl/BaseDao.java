c4addadfebd07eb0142b60f920fea032a3d15fc6
le15874811343 <576169235@qq.com>
2016-09-15 15:18:13 +0800

    hibernate模板基类

--- 前台/S4SSHib/src/cn/com/dao/impl/BaseDao.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/BaseDao.java
@@ -1,10 +1,13 @@
 package cn.com.dao.impl;
 
 import org.springframework.orm.hibernate3.HibernateTemplate;
-
+/**
+ * 
+ * hibernate模板基类
+ */
 public class BaseDao {
     private HibernateTemplate hibernateTemplate=null;
-
+       //hibernate模板类的引用
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
