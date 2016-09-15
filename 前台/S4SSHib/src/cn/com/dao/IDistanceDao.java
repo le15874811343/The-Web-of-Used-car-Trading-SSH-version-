6396556ce93c296d4f86a93d8d37fb31cfcea4df
le15874811343 <576169235@qq.com>
2016-09-15 13:56:55 +0800

    行驶距离操作接口

--- 前台/S4SSHib/src/cn/com/dao/IDistanceDao.java
+++ 前台/S4SSHib/src/cn/com/dao/IDistanceDao.java
@@ -1,6 +1,15 @@
 package cn.com.dao;
 import java.util.*;
 import cn.com.pojo.*;
+/**
+ * 行驶距离操作接口
+ *@author lej 
+ */
 public interface IDistanceDao {
+     /**
+    * 
+    * 按热度获取行驶距离信息的方法
+    *@return Map<Integer,Distance> 
+    */
    public Map<Integer, Distance> getDistanceByCount();
 }
