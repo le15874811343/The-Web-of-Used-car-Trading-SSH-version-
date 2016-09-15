0425e9713a85c3653c6e7ca92d3cc260fa69c258
le15874811343 <576169235@qq.com>
2016-09-15 14:09:05 +0800

    个人需求信息操作接口

--- 前台/S4SSHib/src/cn/com/dao/IPersonNeedDao.java
+++ 前台/S4SSHib/src/cn/com/dao/IPersonNeedDao.java
@@ -1,9 +1,27 @@
 package cn.com.dao;
 
 import cn.com.pojo.*;
-
+/**
+ * 个人需求信息操作接口
+ *@author 
+ */
 public interface IPersonNeedDao {
+  /**
+   * 
+   * 添加个人需求信息的方法
+   *@return int 
+   */
 public int addPersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 删除个人需求信息的方法
+   *@return int 
+   */
 public int deletePersonNeed(Personneed personNeed);
+  /**
+   * 
+   * 获取个人需求信息的方法
+   *@return PersonNeed
+   */
 public Personneed getPerSonNeed(Personneed personNeed);
 }
