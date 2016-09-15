39e8ef454ccb9b389d652cc1b047499d68b19710
le15874811343 <576169235@qq.com>
2016-09-15 18:32:10 +0800

    价格区间服务实现类

--- 前台/S4SSHib/src/cn/com/service/impl/PriceIntervalServiceImpl.java
+++ 前台/S4SSHib/src/cn/com/service/impl/PriceIntervalServiceImpl.java
@@ -6,8 +6,12 @@ import cn.com.pojo.*;
 import cn.com.dao.IPriceIntervalDao;
 import cn.com.dao.impl.PriceIntervalDaoImpl;
 import cn.com.service.IPriceIntervalService;
-
+/**
+ * 价格区间服务实现类
+ * @author lej
+ */
 public class PriceIntervalServiceImpl implements IPriceIntervalService{
+	//价格区间操作接口的引用
    private IPriceIntervalDao intervalDao=null;
    
 	public IPriceIntervalDao getIntervalDao() {
@@ -17,7 +21,11 @@ public class PriceIntervalServiceImpl implements IPriceIntervalService{
 public void setIntervalDao(IPriceIntervalDao intervalDao) {
 	this.intervalDao = intervalDao;
 }
-
+  /**
+   * 
+   * 按热度获取价格区间信息的服务
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, Priceinterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
