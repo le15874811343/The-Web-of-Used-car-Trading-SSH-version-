920c3a4bd4bcbac8cc08d4f95dc26fae96302d5e
le15874811343 <576169235@qq.com>
2016-09-15 15:57:53 +0800

    汽车基本信息操作实现类

--- 前台/S4SSHib/src/cn/com/dao/impl/BasicInfoDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/BasicInfoDaoImpl.java
@@ -7,23 +7,33 @@ import org.hibernate.Hibernate;
 import cn.com.pojo.*;
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 汽车基本信息操作实现类
+ * @author lej
+ */
 public class BasicInfoDaoImpl extends BaseDao implements IBasicInfoDao{
-
+/**
+ * 获取所有基本信息的方法
+ * @return Map<Long, BasicInfo>
+*/
 	@Override
 	public Map<Long, Basicinfo> getAllBasic() {
 		// TODO Auto-generated method stub
 		Map<Long, Basicinfo> mapBasicInfo=new HashMap<Long, Basicinfo>();
 		
 	 String hql="from Basicinfo";
-	List<Basicinfo> baslist=	super.getHibernateTemplate().find(hql);
-	for(Basicinfo b:baslist){
+	List<Basicinfo> baslist=	super.getHibernateTemplate().find(hql);    //获取结果集
+	//将结果添加进Map中
+	for(Basicinfo b:baslist){  
 		mapBasicInfo.put(b.getCId(), b);
 	}
 		
 		return mapBasicInfo;
 	}
-
+/**
+ *根据编号获取汽车基本信息的方法 
+ *@return BasicInfo
+ */
 	@Override
 	public Basicinfo getAllBasicById(Carinfo carInfo) {
 		// TODO Auto-generated method stub
@@ -31,7 +41,7 @@ public class BasicInfoDaoImpl extends BaseDao implements IBasicInfoDao{
 	
 		String hql="from Basicinfo where UId=? and CId=?";
 		try{
-		basicInfo2=	 (Basicinfo) super.getHibernateTemplate().find(hql,new Object[]{carInfo.getUId(),carInfo.getCId()}).get(0);
+		basicInfo2=(Basicinfo) super.getHibernateTemplate().find(hql,new Object[]{carInfo.getUId(),carInfo.getCId()}).get(0); //获取结果
 		}
 		catch (Exception e) {
 			// TODO: handle exception
@@ -39,14 +49,17 @@ public class BasicInfoDaoImpl extends BaseDao implements IBasicInfoDao{
 		
 		return basicInfo2;
 	}
-
+/**
+ * 添加汽车基本信息的方法
+ * @return int
+ */
 	@Override
 	public int addBasicInfo(Basicinfo basicInfo) {
 		// TODO Auto-generated method stub
 	
 		int count=0;
 		try{
-			super.getHibernateTemplate().save(basicInfo);
+			super.getHibernateTemplate().save(basicInfo); //添加进数据库中
 		count=1;
 		}
 		catch (Exception e) {
@@ -54,13 +67,16 @@ public class BasicInfoDaoImpl extends BaseDao implements IBasicInfoDao{
 		}
 		return count;
 	}
-
+/**
+ * 修改汽车基本信息的方法
+ * @return int
+ */
 	@Override
 	public int updateBasicInfo(Basicinfo basicInfo) {
 		// TODO Auto-generated method stub
 	
 		StringBuffer sql=new StringBuffer("update  Basicinfo set aidd=?,srdt=?,bodycolor=?,interiorcolor=?,domf=?,orgin=?,cimd=? where UId=? and CId=? ");
-	
+	      //返回受影响的行数
 		return super.getHibernateTemplate().bulkUpdate(sql.toString(), new Object[]{basicInfo.getAidd(),basicInfo.getSrdt(),basicInfo.getBodycolor(),basicInfo.getInteriorcolor(),
 			basicInfo.getDomf(),basicInfo.getOrgin(),basicInfo.getCimd(),basicInfo.getUId(),basicInfo.getCId()});
 	}
