bf0c1abd322bfc85bf4c1e09df2989b9f5b109a4
le15874811343 <576169235@qq.com>
2016-09-15 17:42:39 +0800

    排放标准服务接口

--- 前台/S4SSHib/src/cn/com/service/IEmissionstandardService.java
+++ 前台/S4SSHib/src/cn/com/service/IEmissionstandardService.java
@@ -3,7 +3,15 @@ package cn.com.service;
 import java.util.*;
 
 import cn.com.pojo.*;
-
+/**
+ * 
+ * 排放标准服务接口
+ *@author  lej
+ */
 public interface IEmissionstandardService {
+	/**
+	 * 按热度获取排放标准信息的服务
+	 * @Map<Integer,Emissionstandard>
+	 */
 	public Map<Integer, Emissionstandard> getEmissionstandardByCount();
 }
