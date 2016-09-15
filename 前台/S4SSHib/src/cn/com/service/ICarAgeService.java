24bac23857903041eae1a7938e94dd0db12718bf
le15874811343 <576169235@qq.com>
2016-09-15 17:35:28 +0800

    车龄信息服务接口

--- 前台/S4SSHib/src/cn/com/service/ICarAgeService.java
+++ 前台/S4SSHib/src/cn/com/service/ICarAgeService.java
@@ -2,7 +2,14 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 车龄信息服务接口
+ *@author  lej 
+ */
 public interface ICarAgeService {
+	/**
+	 * 按热度获取车龄信息的服务
+	 * @return Map<Integer,CarAge>
+	 */
 	public Map<Integer,Carage> getCarAgeByCount();
 }
