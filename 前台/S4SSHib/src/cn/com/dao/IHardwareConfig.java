ee586159f8b5e945065c6273b9e97baeaf1b57fe
le15874811343 <576169235@qq.com>
2016-09-15 13:59:08 +0800

    汽车硬件配置信息操作接口

--- 前台/S4SSHib/src/cn/com/dao/IHardwareConfig.java
+++ 前台/S4SSHib/src/cn/com/dao/IHardwareConfig.java
@@ -2,10 +2,33 @@ package cn.com.dao;
 
 import java.util.Map;
 import cn.com.pojo.*;
-
+/**
+ * 汽车硬件配置信息操作接口
+ *@author lej 
+ */
 public interface IHardwareConfig {
+   /**
+   * 
+   * 获取所有汽车硬件配置信息的方法
+   *@return Map<Long,HardwareConfig> 
+   */
 public Map<Long, Hardwareconfig> getAllHardwareConfig();
+/**
+ * 按编号获取汽车硬件配置信息的方法
+ * @parma carInfo
+ * @return HardwareConfig 
+ */
 public Hardwareconfig getHardwareConfigById(Carinfo carInfo);
+/**
+ * 添加汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 public int addHardwareConfig(Hardwareconfig hardwareConfig);
+/**
+ * 修改汽车硬件配置信息的方法
+ * @parma hardwareConfig
+ * @return int
+ */
 public int updateHardwareConfig(Hardwareconfig hardwareConfig);
 }
