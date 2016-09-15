9233e591cb3533f6987dae598e74d89b1c4641f9
le15874811343 <576169235@qq.com>
2016-09-15 16:36:22 +0800

    按热度获取车型信息的方法

--- 前台/S4SSHib/src/cn/com/dao/impl/CarTypeDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/CarTypeDaoImpl.java
@@ -5,9 +5,15 @@ import cn.com.util.*;
 import cn.com.dao.*;
 import java.util.*;
 import java.sql.*;
-
+/**
+ * 车型操作实现类
+ * @author
+ */
 public class CarTypeDaoImpl extends BaseDao implements ICarTypeDao {
-
+        /**
+	 * 按热度获取车型信息的方法
+	 * @return Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, Cartype> getCarTypeByCount() {
 		// TODO Auto-generated method stub
@@ -15,7 +21,8 @@ public class CarTypeDaoImpl extends BaseDao implements ICarTypeDao {
 		
    String hql="from Cartype order by TCount";
 		try {
-			List<Cartype> catplist = PageUtil.querylist(1, 6, hql, null);
+			List<Cartype> catplist = PageUtil.querylist(1, 6, hql, null); //获取指定行数区间的车型信息
+			//遍历结果集，加入map中
 			for (Cartype c : catplist) {
 				carTypeMap.put(c.getTId(), c);
 			}
@@ -25,16 +32,21 @@ public class CarTypeDaoImpl extends BaseDao implements ICarTypeDao {
 		}
 		return carTypeMap;
 	}
-
+        /**
+	 * 获取所有车型信息的方法
+	 * @return  Map<Integer,CarType>
+	 */
 	@Override
 	public Map<Integer, Cartype> getAllCarType() {
 		// TODO Auto-generated method stub
 		Map<Integer, Cartype> carTypeMap = new HashMap<Integer, Cartype>();
 		String sql = "from Cartype where 1=1 ";
 
-		try {
+		try {   
+			//获取结果集
 			List<Cartype> catplist = super.getHibernateTemplate().find(
 					sql);
+					//遍历结果集，加入map中
 			for (Cartype c : catplist) {
 				carTypeMap.put(c.getTId(), c);
 			}
