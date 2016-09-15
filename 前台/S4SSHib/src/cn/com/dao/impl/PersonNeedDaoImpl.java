9e4389293f724efefefc22a2965719dc270fad84
le15874811343 <576169235@qq.com>
2016-09-15 16:59:18 +0800

    个人需求信息操作实现类

--- 前台/S4SSHib/src/cn/com/dao/impl/PersonNeedDaoImpl.java
+++ 前台/S4SSHib/src/cn/com/dao/impl/PersonNeedDaoImpl.java
@@ -8,23 +8,23 @@ import cn.com.util.*;
 import cn.com.pojo.*;
 import cn.com.dao.*;
 
-
+/**
+ * 个人需求信息操作实现类
+ *@author 
+ */
 public class PersonNeedDaoImpl extends BaseDao implements IPersonNeedDao,IPageDao{
 
-//	 private long p_id;
-//	 private long u_id;
-//	 private String p_brand;
-//	 private String p_series;
-//	 private String p_age;
-//	 private String p_price;
-//	 private String p_time;
-//	 private String p_miaoshu;
+  /**
+   * 
+   * 添加个人需求信息的方法
+   *@return int 
+   */
 	public int addPersonNeed(Personneed personNeed) {
 		// TODO Auto-generated method stub
 	
 		int count=0;
 		try{
-			super.getHibernateTemplate().save(personNeed);
+			super.getHibernateTemplate().save(personNeed); //加入
 			count=1;
 		}
 		catch (Exception e) {
@@ -33,12 +33,16 @@ public class PersonNeedDaoImpl extends BaseDao implements IPersonNeedDao,IPageDa
 		}
 		return count;
 	}
-
+  /**
+   * 
+   * 删除个人需求信息的方法
+   *@return int 
+   */
 	public int deletePersonNeed(Personneed personNeed) {
 		// TODO Auto-generated method stub
 		int count=0;
 		try{
-			super.getHibernateTemplate().delete(personNeed);
+			super.getHibernateTemplate().delete(personNeed); //删除
 			count=1;
 		}
 		catch (Exception e) {
@@ -47,13 +51,18 @@ public class PersonNeedDaoImpl extends BaseDao implements IPersonNeedDao,IPageDa
 		}
 		return count;
 	}
-
+  /**
+   * 
+   * 获取个人需求信息的方法
+   *@return PersonNeed
+   */
 	@Override
 	public Personneed getPerSonNeed(Personneed personNeed) {
 		// TODO Auto-generated method stub
 		
 		Personneed _PersonNeed=null;
 		StringBuffer sql=new StringBuffer(" from Personneed where 1=1 ");
+		//动态准备参数并延伸HQL语句
 		List<Object> parmas=new ArrayList<Object>();
 		if(personNeed.getUId()!=null){
 			sql.append(" and UId=? ");
@@ -87,15 +96,16 @@ if(personNeed.getPState()!=null){
 	sql.append(" and PState=? ");
 	parmas.add(personNeed.getPState());
 }
+//压入确定有的参数
 Object[] o=new Object[parmas.size()];
 for(int i=0;i<parmas.size();i++){
 	o[i]=parmas.get(i);		
 	}
        
 		try {
-			List<Personneed> plist=(List<Personneed>) super.getHibernateTemplate().find(sql.toString(),o);
+			List<Personneed> plist=(List<Personneed>) super.getHibernateTemplate().find(sql.toString(),o);//获取结果集
 			if(plist.size()>0){
-				_PersonNeed=plist.get(0);
+				_PersonNeed=plist.get(0); //获取结果集对象
 			}
 			
 		} catch (Exception e) {
@@ -119,25 +129,31 @@ for(int i=0;i<parmas.size();i++){
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+        /**
+         * 获取个人需求记录总条数
+         * 
+         * @return int
+         */
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		Long count=null;
 		Personneed personNeed=(Personneed) object;
 		StringBuffer sql=new StringBuffer("select count(*) from Personneed where 1=1");
+		//动态准备参数并延伸hql语句
 		List<Object> parmas=new ArrayList<Object>();
 		if(personNeed.getUId()!=0){
 			sql.append(" and UId=?");
 			parmas.add(personNeed.getUId());
 		}
+		//压入确定有的参数
 		Object[] o=new Object[parmas.size()];
 		for(int i=0;i<parmas.size();i++){
 			o[i]=parmas.get(i);		
 			}
 
 	try {
-		 count= (Long) super.getHibernateTemplate().find(sql.toString(),o).listIterator().next();
+		 count= (Long) super.getHibernateTemplate().find(sql.toString(),o).listIterator().next(); //获取结果
 	
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
@@ -145,7 +161,12 @@ for(int i=0;i<parmas.size();i++){
 	}
 		return count;
 	}
-
+      /**
+       * 分页获取个人需求信息
+       * @param curPage 当前页
+       * @param rowsPrePage 每页显示记录条数
+       * @return Map<Long,Object>
+       */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
@@ -153,6 +174,7 @@ for(int i=0;i<parmas.size();i++){
 		Personneed personNeed=(Personneed) object;
 		 StringBuffer sql=new StringBuffer(" from Personneed  a  where 1=1");
 		 Map<Long, Object> personNeedMap=new HashMap<Long, Object>();
+		 	//动态准备参数并延伸hql语句
 			List<Object> params=new ArrayList<Object>();
 			if(personNeed.getUId()!=0){
 				sql.append(" and UId=?");
@@ -163,6 +185,7 @@ for(int i=0;i<parmas.size();i++){
 		
 			
 		    try {
+		    	//获取指定行数区间符合条件的结果集
 		   List<Personneed> plist= 	PageUtil.querylist(curPage, rowsPrePage, sql.toString(), params);
 		   for(Personneed p:plist){
 			   personNeedMap.put(p.getPId(), p);
