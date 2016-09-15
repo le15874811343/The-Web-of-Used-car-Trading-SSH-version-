1f52b564780a85cbd3fd71c2da1a7e533107a7f0
le15874811343 <576169235@qq.com>
2016-09-15 14:09:34 +0800

    价格区间操作接口

--- 前台/S4SSHib/src/cn/com/dao/IPriceIntervalDao.java
+++ 前台/S4SSHib/src/cn/com/dao/IPriceIntervalDao.java
@@ -2,6 +2,15 @@ package cn.com.dao;
 
 import java.util.Map;
 import cn.com.pojo.*;
+/**
+ * 价格区间操作接口
+ * @author lej
+ */
 public interface IPriceIntervalDao {
+  /**
+   * 
+   * 按热度获取价格区间信息的方法
+   * @return Map<Integer,PriceInterval>
+   */
   public Map<Integer,Priceinterval> getPriceIntervalByCount();
 }
