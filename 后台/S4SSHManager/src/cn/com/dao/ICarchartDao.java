b3d4d813fbc9f2af6eaf7a63f6207d594e1c3510
le15874811343 <576169235@qq.com>
2016-09-16 23:35:11 +0800

    品牌销量操作接口

--- 后台/S4SSHManager/src/cn/com/dao/ICarchartDao.java
+++ 后台/S4SSHManager/src/cn/com/dao/ICarchartDao.java
@@ -4,10 +4,29 @@ import java.util.List;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 品牌销量操作接口
+ * 
+ */
 public interface ICarchartDao {
+  /**
+   * 按年月查询的方法
+   * 
+   */
 public List<Carchart> chartinfo(int year,int month);
+/**
+ * 查询不同年的方法
+ * 
+ */
 public List<Carchart> chartinfo( );
+/**
+ * 按年月日查询的方法
+ * 
+ */
 public Map<String, Carchart> chartinfo(int year,int month,int day);
+/**
+ * 按年查询的方法
+ * 
+ */
 public Map<String, Carchart> chartinfo(int year);
 }
