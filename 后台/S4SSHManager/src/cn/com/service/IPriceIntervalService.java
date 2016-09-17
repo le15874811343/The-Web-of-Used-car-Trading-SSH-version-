e767b834e6879b33310ab60ee1ebcdf83ecb1624
le15874811343 <576169235@qq.com>
2016-09-17 14:48:13 +0800

    价格区间服务接口

--- 后台/S4SSHManager/src/cn/com/service/IPriceIntervalService.java
+++ 后台/S4SSHManager/src/cn/com/service/IPriceIntervalService.java
@@ -3,11 +3,39 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 价格区间服务接口
+ * @author lej
+ */
 public interface IPriceIntervalService {
+  /**
+   * 
+   * 按热度获取价格区间信息的服务
+   * @return Map<Integer,PriceInterval>
+   */
 	public Map<Integer, Priceinterval> getPriceIntervalByCount();
+  /**
+   * 
+   * 添加价格区间信息的服务
+   * @return boolean
+   */
 	public boolean addPriceInterval(Priceinterval priceInterval);
+  /**
+   * 
+   * 删除价格区间信息的服务
+   * @return boolean
+   */
 	  public boolean deletePriceInterval(Priceinterval priceInterval);
+  /**
+   * 
+   * 修改价格区间信息的服务
+   * @return boolean
+   */
 	  public boolean updatePriceInterval(Priceinterval priceInterval);
+  /**
+   * 
+   * 获取价格区间信息的服务
+   * @return PriceInterval
+   */
 	  public Priceinterval getPriceIntervalById(Priceinterval priceInterval);
 }
