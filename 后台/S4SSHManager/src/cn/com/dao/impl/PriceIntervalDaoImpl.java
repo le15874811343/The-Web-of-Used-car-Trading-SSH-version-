cd2ffb3cd4116a575ec6a25648fe25d707d513d5
le15874811343 <576169235@qq.com>
2016-09-17 20:26:14 +0800

    价格区间操作实现类

--- 后台/S4SSHManager/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
+++ 后台/S4SSHManager/src/cn/com/dao/impl/PriceIntervalDaoImpl.java
@@ -7,9 +7,16 @@ import cn.com.pojo.*;
 
 import cn.com.dao.*;
 import cn.com.util.*;
-
+/**
+ * 价格区间操作实现类
+ * @author lej
+ */
 public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,IPageDao{
-
+ /**
+   * 
+   * 按热度获取价格区间信息的方法
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, Priceinterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
@@ -17,7 +24,8 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 	
 	      String hql="from Priceinterval  order by PCount desc";
 	try {
-	List<Priceinterval> plist=	PageUtil.querylist(1, 4, hql, null);
+	List<Priceinterval> plist=	PageUtil.querylist(1, 4, hql, null);//获取指定行数区间的结果集
+	//遍历结果集，加入map中
 	for(Priceinterval p:plist){
 		priceIntervalMap.put(p.getPId(), p);
 	}
@@ -28,13 +36,17 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 	}
 		return priceIntervalMap;
 	}
-
+  /**
+   * 
+   * 删除价格区间信息的方法
+   * @return int
+   */
 	@Override
 	public int addPriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
 		int count = 0;
 		try {
-			super.getHibernateTemplate().save(priceInterval);
+			super.getHibernateTemplate().save(priceInterval);//加入
 			count = 1;
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -43,13 +55,17 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 		}
 		return count;
 	}
-
+ /**
+   * 
+   * 删除价格区间信息的方法
+   * @return int
+   */
 	@Override
 	public int deletePriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
 		int count = 0;
 		try {
-			super.getHibernateTemplate().delete(priceInterval);
+			super.getHibernateTemplate().delete(priceInterval);//删除
 			count = 1;
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -58,14 +74,23 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 		}
 		return count;
 	}
-
+  /**
+   * 
+   * 修改价格区间信息的方法
+   * @return int
+   */
 	@Override
 	public int updatePriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
 		String sql="update Priceinterval set PName=?,PCount=? where PId=?";
+		//返回受影响的行数
 		return super.getHibernateTemplate().bulkUpdate(sql, new Object[]{priceInterval.getPName(),priceInterval.getPCount(),priceInterval.getPId()});
 	}
-
+ /**
+   * 
+   * 获取价格区间信息的方法
+   * @return PriceInterval
+   */
 	@Override
 	public Priceinterval getPriceIntervalById(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
@@ -74,9 +99,9 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 	
 			
 		    try {
-				List<Priceinterval> pri=super.getHibernateTemplate().find(sql, priceInterval.getPId());
+				List<Priceinterval> pri=super.getHibernateTemplate().find(sql, priceInterval.getPId());//获取结果集
 				if(pri.size()>0){
-					_priceInterval=pri.get(0);
+					_priceInterval=pri.get(0);//获取结果
 				}
 				
 			} catch (Exception e) {
@@ -100,14 +125,17 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+    /**
+     * 获取价格区间信息的记录总条数
+     * @return int
+     */		
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		Long count=null;
 		try{
 		StringBuffer sql=new StringBuffer("select count(*) from Priceinterval where 1=1");
-	count=	(Long) super.getHibernateTemplate().find(sql.toString()).iterator().next();
+	count=	(Long) super.getHibernateTemplate().find(sql.toString()).iterator().next();//获取结果
 		}
 		catch (Exception e) {
 			// TODO: handle exception
@@ -115,14 +143,20 @@ public class PriceIntervalDaoImpl extends BaseDao implements IPriceIntervalDao ,
 		}
 		return count;
 	}
-
+ /**
+ * 分页获取价格区间信息
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */	
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		String hql="from Priceinterval  a  where 1=1 order by PCount desc";
 		Map<Long, Object> carBrandMap=new HashMap<Long, Object>();
-	List<Priceinterval> carBrandlist=	PageUtil.querylist(curPage, rowsPrePage, hql, null);
+	List<Priceinterval> carBrandlist=	PageUtil.querylist(curPage, rowsPrePage, hql, null);//获取指定行数区间的结果集
+	//遍历结果集，加入map中
 	for(Priceinterval c:carBrandlist){
 		carBrandMap.put((long) c.getPId(), c);
 	}
