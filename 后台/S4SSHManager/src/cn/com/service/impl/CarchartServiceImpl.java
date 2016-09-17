5d3c71fac15eb5fc1f90e8dd0f6ba396af2a3f87
le15874811343 <576169235@qq.com>
2016-09-17 15:12:43 +0800

    品牌销量服务实现类

--- 后台/S4SSHManager/src/cn/com/service/impl/CarchartServiceImpl.java
+++ 后台/S4SSHManager/src/cn/com/service/impl/CarchartServiceImpl.java
@@ -7,24 +7,43 @@ import cn.com.pojo.Carchart;
 import cn.com.dao.ICarchartDao;
 import cn.com.dao.impl.CarchartDaoImpl;
 import cn.com.service.ICarchartService;
-
+/**
+ * 品牌销量服务实现类
+ * 
+ */
 public class CarchartServiceImpl implements ICarchartService{
-ICarchartDao carchartDao=new CarchartDaoImpl();
-	@Override
+ICarchartDao carchartDao=new CarchartDaoImpl(); //品牌销量操作接口的引用
+  /**
+   * 按年月查询的服务
+   * 
+   */
+	@Override 
 	public List<Carchart> chartinfo(int year, int month) {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo(year, month);
 	}
+/**
+ * 查询不同年的服务
+ * 
+ */
 	@Override
 	public List<Carchart> chartinfo() {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo();
 	}
+/**
+ * 按年月日查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		return carchartDao.chartinfo(year, month, day);
 	}
+/**
+ * 按年查询的服务
+ * 
+ */
 	@Override
 	public Map<String, Carchart> chartinfo(int year) {
 		// TODO Auto-generated method stub
