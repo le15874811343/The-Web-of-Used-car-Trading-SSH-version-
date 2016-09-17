fa3ae124cfdcc6d070584d007f955cf5e2540b1d
le15874811343 <576169235@qq.com>
2016-09-17 18:26:28 +0800

    车龄信息操作实现类

--- 后台/S4SSHManager/src/cn/com/dao/impl/CarAgeDaoImpl.java
+++ 后台/S4SSHManager/src/cn/com/dao/impl/CarAgeDaoImpl.java
@@ -7,29 +7,39 @@ import cn.com.dao.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
-
+/**
+ * 车龄信息操作实现类
+ *@author  lej 
+ */
 public class CarAgeDaoImpl extends BaseDao implements ICarAgeDao,IPageDao{
-  
+        /**
+	 * 按热度获取车龄信息的方法
+	 * @return Map<Integer,CarAge>
+	 */
 	@Override
 	public Map<Integer, Carage> getCarAgeByCount() {
 		// TODO Auto-generated method stub
 		Map<Integer, Carage> carAgeMap=new HashMap<Integer, Carage>();
 	
 	 String hql="from Carage  order by ACount desc";
-	List<Carage> caglist= PageUtil.querylist(1, 5, hql, null);
+	List<Carage> caglist= PageUtil.querylist(1, 5, hql, null);  //获取结果集
+	//遍历结果集并加入到map中
 	for(Carage c:caglist){
 		carAgeMap.put(c.getAId(), c);
 	}
 	
 		return carAgeMap;
 	}
-
+        /**
+	 * 添加车龄信息的方法
+	 * @return   int
+	 */
 	@Override
 	public int addCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
 		int count=0;
 		try{
-			 super.getHibernateTemplate().save(carAge);
+			 super.getHibernateTemplate().save(carAge);   //加入
 			 count=1;
 		}
 		catch (Exception e) {
@@ -39,12 +49,16 @@ public class CarAgeDaoImpl extends BaseDao implements ICarAgeDao,IPageDao{
 		return count;
 	}
 
+	/**
+	 * 删除车龄信息的方法
+	 * @return   int
+	 */
 	@Override
 	public int deleteCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
 		int count=0;
 		try{
-			super.getHibernateTemplate().delete(carAge);
+			super.getHibernateTemplate().delete(carAge); //删除
 			count=1;
 		}
 		catch (Exception e) {
@@ -53,24 +67,31 @@ public class CarAgeDaoImpl extends BaseDao implements ICarAgeDao,IPageDao{
 		}
 		return count;
 	}
+        /**
+	 * 修改车龄信息的方法
+	 * @return   int
+	 */
 
 	@Override
 	public int updateCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
 		String sql="update Carage set AName=?,ACount=? where AId=?";
-		
+		//返回受影响的行数
 		return super.getHibernateTemplate().bulkUpdate(sql.toString(), new Object[]{carAge.getAName(),carAge.getACount(),carAge.getAId()});
 	}
-
+        /**
+	 * 获取车龄信息的方法
+	 * @return CarAge
+	 */
 	@Override
 	public Carage getCarAgeById(Carage carAge) {
 		// TODO Auto-generated method stub
 		String sql=" from Carage where AId=?";
 		Carage _Carage=null;
 		try{
-	List<Carage> clist=		super.getHibernateTemplate().find(sql, carAge.getAId());
+	List<Carage> clist=		super.getHibernateTemplate().find(sql, carAge.getAId()); //获取结果集
 	if(clist.size()>0){
-		_Carage=clist.get(0);
+		_Carage=clist.get(0); //获取结果
 	}
 		}
 		catch (Exception e) {
@@ -93,7 +114,10 @@ public class CarAgeDaoImpl extends BaseDao implements ICarAgeDao,IPageDao{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+     /**
+     * 获取车龄信息的记录总条数
+     * @return Long
+     */
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
@@ -102,7 +126,7 @@ Long count=null;
 		
 		StringBuffer sql=new StringBuffer("select count(*) from Carage where 1=1");
 		try{
-	count=   (Long) super.getHibernateTemplate().find(sql.toString()).iterator().next();
+	count=   (Long) super.getHibernateTemplate().find(sql.toString()).iterator().next(); //获取结果
 		}
 		catch (Exception e) {
 			// TODO: handle exception
@@ -110,14 +134,20 @@ Long count=null;
 		}
 		return count;
 	}
-
+/**
+ * 分页获取车龄信息
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		 StringBuffer sql=new StringBuffer("from Carage  a  where 1=1 order by ACount desc");
 		 Map<Long, Object> brandMap=new HashMap<Long, Object>();
-		List<Carage> agel=	PageUtil.querylist(curPage, rowsPrePage, sql.toString(), null);
+		List<Carage> agel=	PageUtil.querylist(curPage, rowsPrePage, sql.toString(), null); //获取指定行数区间的结果集
+	       //遍历结果集，加入map中
 		for(Carage c:agel){
 			brandMap.put((long) c.getAId(), c);
 		}
