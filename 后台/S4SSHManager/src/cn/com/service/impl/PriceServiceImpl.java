221b8cc24081b8a876f6960a80f9919165f69601
le15874811343 <576169235@qq.com>
2016-09-17 15:35:00 +0800

    按年月日查询的服务

--- 后台/S4SSHManager/src/cn/com/service/impl/PriceServiceImpl.java
+++ 后台/S4SSHManager/src/cn/com/service/impl/PriceServiceImpl.java
@@ -5,27 +5,43 @@ import cn.com.pojo.Price;
 import cn.com.dao.IPriceDao;
 import cn.com.dao.impl.PriceDaoImpl;
 import cn.com.service.IPriceService;
-
+/**
+ * 营业额查询服务实现类
+ * 
+ */
 public class PriceServiceImpl implements IPriceService {
-private IPriceDao priceDao=new PriceDaoImpl();
+private IPriceDao priceDao=new PriceDaoImpl(); //营业额查询操作接口的引用
+        /**
+	 * 按年月日查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year, int month, int day) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month, day);
 	}
-
+        /**
+	 * 按年月查询的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo(int year, int month) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year, month);
 	}
-
+        /**
+	 * 按年查询的服务
+	 * 
+	 */
 	@Override
 	public  List<Price> getsellinfo(int year) {
 		// TODO Auto-generated method stub
 		return priceDao.getsellinfo(year);
 	}
-
+        /**
+	 * 查询不同年营业额的服务
+	 * 
+	 */
 	@Override
 	public List<Price> getsellinfo() {
 		// TODO Auto-generated method stub
