1303f79e196dde315eda319659b5f9a50e8b4c5b
le15874811343 <576169235@qq.com>
2016-09-17 14:59:40 +0800

    车龄信息服务实现类

--- 后台/S4SSHManager/src/cn/com/service/impl/CarAgeServiceImpl.java
+++ 后台/S4SSHManager/src/cn/com/service/impl/CarAgeServiceImpl.java
@@ -6,8 +6,14 @@ import cn.com.pojo.*;
 
 import cn.com.dao.*;
 import cn.com.service.*;
+/**
+ * 车龄信息服务实现类
+ *@author  lej 
+ */
 public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
+	//车辆信息操作接口的引用
   private ICarAgeDao carAgeDao=null;
+  //分页处理操作接口的引用
 	private IPageDao caragePage=null;
 	
 	public ICarAgeDao getCarAgeDao() {
@@ -25,7 +31,10 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 	public void setCaragePage(IPageDao caragePage) {
 		this.caragePage = caragePage;
 	}
-
+        /**
+	 * 按热度获取车龄信息的服务
+	 * @return Map<Integer,CarAge>
+	 */
 	@Override
 	public Map<Integer, Carage> getCarAgeByCount() {
 		// TODO Auto-generated method stub
@@ -46,13 +55,21 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+    /**
+     * 获取车龄信息的记录总条数的服务
+     * @return int
+     */
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return caragePage.queryPersonCarCount(object);
 	}
-	
+/**
+ * 分页获取车龄信息的服务
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
@@ -60,6 +77,10 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 		
 		return caragePage.showPersonCarList(curPage, rowsPrePage, object);
 	}
+	/**
+	 * 添加车龄信息的服务
+	 * @return   boolean
+	 */
 	@Override
 	public boolean addCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
@@ -69,6 +90,10 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 		}
 		return flag;
 	}
+	/**
+	 * 删除车龄信息的服务
+	 * @return   boolean
+	 */
 	@Override
 	public boolean deleteCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
@@ -78,6 +103,10 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 		}
 		return flag;
 	}
+	/**
+	 * 修改车龄信息的服务
+	 * @return   boolean
+	 */
 	@Override
 	public boolean updateCarAge(Carage carAge) {
 		// TODO Auto-generated method stub
@@ -87,7 +116,10 @@ public class CarAgeServiceImpl implements ICarAgeService,IPageDao {
 		}
 		return flag;
 	}
-
+        /**
+	 * 获取车龄信息的服务
+	 * @return CarAge
+	 */
 	@Override
 	public Carage getCarAgeById(Carage carAge) {
 		// TODO Auto-generated method stub
