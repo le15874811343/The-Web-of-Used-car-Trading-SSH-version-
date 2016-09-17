7e422fac1ba3587a6ecc2c2930e22daf18db5ac2
le15874811343 <576169235@qq.com>
2016-09-17 15:29:13 +0800

    价格区间服务实现类

--- 后台/S4SSHManager/src/cn/com/service/impl/PriceIntervalServiceImpl.java
+++ 后台/S4SSHManager/src/cn/com/service/impl/PriceIntervalServiceImpl.java
@@ -8,10 +8,13 @@ import cn.com.dao.IPageDao;
 import cn.com.dao.IPriceIntervalDao;
 
 import cn.com.service.IPriceIntervalService;
-
+/**
+ * 价格区间服务实现类
+ * @author lej
+ */
 public class PriceIntervalServiceImpl implements IPriceIntervalService,IPageDao{
-   private IPriceIntervalDao intervalDao=null;
-   private IPageDao  priceIntPage=null;
+   private IPriceIntervalDao intervalDao=null; //价格区间操作接口的引用
+   private IPageDao  priceIntPage=null; //分页处理操作接口的引用
 	
 	public IPriceIntervalDao getIntervalDao() {
 	return intervalDao;
@@ -28,7 +31,11 @@ public IPageDao getPriceIntPage() {
 public void setPriceIntPage(IPageDao priceIntPage) {
 	this.priceIntPage = priceIntPage;
 }
-
+ /**
+   * 
+   * 按热度获取价格区间信息的服务
+   * @return Map<Integer,PriceInterval>
+   */
 	@Override
 	public Map<Integer, Priceinterval> getPriceIntervalByCount() {
 		// TODO Auto-generated method stub
@@ -49,19 +56,32 @@ public void setPriceIntPage(IPageDao priceIntPage) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+    /**
+     * 获取价格区间信息的记录总条数
+     * @return int
+     */			
 	@Override
 	public Long queryPersonCarCount(Object object) {
 		// TODO Auto-generated method stub
 		return priceIntPage.queryPersonCarCount(object);
 	}
-	
+/**
+ * 分页获取价格区间信息
+ * @param curPage 当前页数
+ * @param rowsPrePage
+ * @return Map<Long,Object>
+ */		
 	@Override
 	public Map<Long, Object> showPersonCarList(int curPage, int rowsPrePage,
 			Object object) {
 		// TODO Auto-generated method stub
 		return priceIntPage.showPersonCarList(curPage, rowsPrePage, object);
 	}
+  /**
+   * 
+   * 添加价格区间信息的服务
+   * @return boolean
+   */
 	@Override
 	public boolean addPriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
@@ -71,6 +91,11 @@ public void setPriceIntPage(IPageDao priceIntPage) {
 		}
 		return flag;
 	}
+  /**
+   * 
+   * 删除价格区间信息的服务
+   * @return boolean
+   */
 	@Override
 	public boolean deletePriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
@@ -80,6 +105,11 @@ public void setPriceIntPage(IPageDao priceIntPage) {
 		}
 		return flag;
 	}
+ /**
+   * 
+   * 修改价格区间信息的服务
+   * @return boolean
+   */
 	@Override
 	public boolean updatePriceInterval(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
@@ -89,7 +119,11 @@ public void setPriceIntPage(IPageDao priceIntPage) {
 		}
 		return flag;
 	}
-
+  /**
+   * 
+   * 修改价格区间信息的服务
+   * @return boolean
+   */
 	@Override
 	public Priceinterval getPriceIntervalById(Priceinterval priceInterval) {
 		// TODO Auto-generated method stub
