59e4cc114831c71efb58274d27ebc915f0d8d065
le15874811343 <576169235@qq.com>
2016-09-17 14:43:38 +0800

    车系信息服务接口

--- 后台/S4SSHManager/src/cn/com/service/IModelService.java
+++ 后台/S4SSHManager/src/cn/com/service/IModelService.java
@@ -3,10 +3,33 @@ package cn.com.service;
 import java.util.*;
 
 import cn.com.pojo.*;
-
+/**
+ * 车系信息服务接口
+ * @author lej
+ */
 public interface IModelService {
+      /**
+       * 按条件获取车系信息的服务
+       * @parma model
+       *@return List<Model> 
+       */
 	public List<Model> getModelByWhere(Model model);
+      /**
+       * 添加车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean addModel(Model model);
+      /**
+       * 修改车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean updateModel(Model model);
+      /**
+       * 删除车系信息的服务
+       * @parma model
+       *@return boolean
+       */
 	public boolean deleteModel(Model model);
 }
