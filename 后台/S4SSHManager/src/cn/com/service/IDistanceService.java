e6530c51464bbc176a2a47c3d147a4bc3808160c
le15874811343 <576169235@qq.com>
2016-09-17 14:40:25 +0800

    行驶距离服务接口

--- 后台/S4SSHManager/src/cn/com/service/IDistanceService.java
+++ 后台/S4SSHManager/src/cn/com/service/IDistanceService.java
@@ -3,11 +3,39 @@ package cn.com.service;
 import java.util.Map;
 
 import cn.com.pojo.*;
-
+/**
+ * 行驶距离服务接口
+ *@author lej 
+ */
 public interface IDistanceService {
+   /**
+    * 
+    * 按热度获取行驶距离信息的服务
+    *@return Map<Integer,Distance> 
+    */
 	public Map<Integer, Distance> getDistanceByCount();
+	 /**
+           * 添加行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	 public boolean addDistance(Distance distance);
+	  /**
+           * 删除行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	   public boolean deleteDistance(Distance distance);
+	  /**
+           * 修改行驶距离的服务
+           * @parma distance
+           * @return boolean
+           */
 	   public boolean updateDistance(Distance distance);
+   /**
+    * 
+    * 按编号获取行驶距离信息的服务
+    *@return Distance
+    */
 	   public Distance getDistanceById(Distance distance);
 }
